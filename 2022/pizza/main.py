a0ea52262c434ee2a820e6976066b88c81b8e21f
Margot Bonilla <margotbonilla@fb.com>
2022-02-19 09:01:01 +0100

    [2022-pizza] Create boilerparte

---
+++ 2022/pizza/main.py
@@ -0,0 +1,124 @@
+import sys
+
+
+class Clients:
+    def __init__(self):
+        self.clients = list()
+
+    def append(self, client):
+        self.clients.append(client)
+
+    def __str__(self):
+        return '\n\n'.join([str(client) for client in self.clients])
+
+    def __repr__(self):
+        return '\n\n'.join([str(client) for client in self.clients])
+
+
+class Client:
+    def __init__(self, likes, dislikes):
+        self.likes = likes
+        self.dislikes = dislikes
+
+    def __str__(self):
+        return f'likes: {",".join(self.likes)}\ndislikes: {",".join(self.dislikes)}'
+
+    def __repr__(self):
+        return f'likes: {",".join(self.likes)}\ndislikes: {",".join(self.dislikes)}'
+
+
+def read_file(in_file):
+    """
+    Read Input File
+
+    Args:
+        in_file: input file path
+    
+    Returns:
+        List of Clients
+    """
+    # Define variables
+    clients = Clients()
+
+    # Read the file into variables    
+    with open(in_file, 'r') as infile:
+        number_of_clients = int(infile.readline())
+        for client in range(number_of_clients):
+            likes = [ingredient for ingredient in infile.readline().strip().split(' ')[1:]]
+            dislikes = [ingredient for ingredient in infile.readline().strip().split(' ')[1:]]
+            clients.append(Client(likes, dislikes))
+
+    print(clients)
+    return clients
+
+
+def process(result_params):
+    """
+    The main program reads the input file, processes the calculation
+    and writes the output file
+
+    Args:
+        result_params: Resulted Parameters  
+
+    Returns:
+        final_score: Final Score
+        , final_result: Final Result    
+    """
+    final_score = 0
+    final_result = []
+
+    ### Logic here
+
+    return final_score, final_result    # return process result
+
+
+def write_file(out_file, final_result):
+    """
+    Write the submission file
+
+    Args:
+        out_file: output file path
+    """
+    with open(out_file, 'w') as outfile:
+        # Save result into the output file
+        pass
+
+
+def main(in_file, out_file):
+    """
+    The main program reads the input file, processes the calculation
+    and writes the output file
+
+    Args:
+        in_file: input file path
+        out_file: output file path
+    """
+
+    # Read File
+    result_params = read_file(in_file)
+    # Process Algorithm
+    final_score, final_result = process(result_params)
+    # Print Score
+    print('Score: {}'.format(final_score))
+    # Save results into the output if instructed
+    if out_file is not None:
+        write_file(out_file, final_result)
+        print('{} is saved. The program completed.'.format(out_file))
+    else:
+        print('The program completed.')
+
+
+if __name__ == "__main__":
+    # test
+    main('input_data/a_an_example.in.txt', None)
+
+
+    # Check arguments
+    # if len(sys.argv) < 2:
+    #     print(sys.argv[0] + ' [in file] [out file: optional]')
+    # elif len(sys.argv) == 2:
+    #     main(sys.argv[1], None)
+    # else:
+    #     main(sys.argv[1], sys.argv[2])
+
+### End of Program ###
