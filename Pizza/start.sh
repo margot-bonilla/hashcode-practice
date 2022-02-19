160e552851e4e1804abd869f9f67da52aab89e83
Moein Akbarof <moein@tropee.co>
2022-02-19 11:30:20 +0100

    Adding multi process

--- Pizza/start.sh
+++ Pizza/start.sh
@@ -1,5 +1,6 @@
-python3 main.py input_data/a_an_example.in.txt output_data/a_an_example.out.txt
-python3 main.py input_data/b_basic.in.txt output_data/b_basic.out.txt
-python3 main.py input_data/c_coarse.in.txt output_data/c_coarse.out.txt
-python3 main.py input_data/d_difficult.in.txt output_data/d_difficult.out.txt
-python3 main.py input_data/e_elaborate.in.txt output_data/e_elaborate.out.txt
+pc=$1
+python3 main.py input_data/a_an_example.in.txt ${pc} output_data/a_an_example.out.txt
+python3 main.py input_data/b_basic.in.txt ${pc} output_data/b_basic.out.txt
+python3 main.py input_data/c_coarse.in.txt ${pc} output_data/c_coarse.out.txt
+python3 main.py input_data/d_difficult.in.txt ${pc} output_data/d_difficult.out.txt
+python3 main.py input_data/e_elaborate.in.txt ${pc} output_data/e_elaborate.out.txt
